7a0bd5ac863977358bd094513b52f35f7c8171c2
Chris Gill <gillehz@gmail.com>
2013-05-22 21:59:41 +0100

    Testing

--- application/controllers/match.php
+++ application/controllers/match.php
@@ -19,7 +19,7 @@ class Match extends CI_Controller {
 			$data['match_id'] = $id;
 	        return $this->load->view($view, $data);
     	} else {
-    		return $this->load->view('welcome', $data);
+    		return $this->load->view('welcome');
     	}
 	}
 }
