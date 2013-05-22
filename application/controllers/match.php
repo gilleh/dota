3e14958bd3ff16323dd9b737c70c44d24689b018
Chris Gill <gillehz@gmail.com>
2013-05-22 21:56:33 +0100

    Testing

--- application/controllers/match.php
+++ application/controllers/match.php
@@ -4,20 +4,20 @@ class Match extends CI_Controller {
 
 	public function index($id = FALSE)
 	{		
-		echo $id;
-		$this->load->helper('xml');
-		$url = "heroes.xml";
-		$xml = file_get_contents($url);
-		$data['heroes'] = new SimpleXMLElement($xml);
-		//$url = "match_195937703.xml";
-		$url ="https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/v001/?key=C2F12A9910099D2B914436795669B1F1&format=XML&match_id=".$id;
-		$xml = file_get_contents($url);
-		$data['match'] = new SimpleXMLElement($xml);
-		
-		xml_convert($data['heroes']);
-		xml_convert($data['match']);
-		$data['match_id'] = $id;
-		
-        return $this->load->view('match', $data);
+		if ($id){
+			$this->load->helper('xml');
+			$url = "heroes.xml";
+			$xml = file_get_contents($url);
+			$data['heroes'] = new SimpleXMLElement($xml);
+			//$url = "match_195937703.xml";
+			$url ="https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/v001/?key=C2F12A9910099D2B914436795669B1F1&format=XML&match_id=".$id;
+			$xml = file_get_contents($url);
+			$data['match'] = new SimpleXMLElement($xml);
+			
+			xml_convert($data['heroes']);
+			xml_convert($data['match']);
+			$data['match_id'] = $id;
+	        return $this->load->view('match', $data);
+    	}
 	}
 }
