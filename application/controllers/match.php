de90e344dcdd15303889fe0454ecf8c5115f1c35
Chris Gill <gillehz@gmail.com>
2013-05-22 22:09:26 +0100

    Testing

--- application/controllers/match.php
+++ application/controllers/match.php
@@ -19,7 +19,7 @@ class Match extends CI_Controller {
 			$data['match_id'] = $id;
 	        return $this->load->view($view, $data);
     	} else {
-    		return $this->load->controller('welcome');
+    		header('Location: http://chrisg.eu/dota/')
     	}
 	}
 }
